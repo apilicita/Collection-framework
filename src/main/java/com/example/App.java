44e4c76a1cdc942962544540482b535515c08256
apilicita <lexgemix@gmail.com>
2026-04-24 11:49:37 +0200

    antes de iterar/recorrer las colecciones

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,46 +17,47 @@ public class App {
 		Integer [] arrayNumerosEnteros= {1,2,3,4,5};
 		List<Integer>  numeroEnteros = Arrays.asList(arrayNumerosEnteros);
 				
-		List<Persona> personas = null;
-		
-		var persona1 = Persona.builder()
+		//List<Persona> personas = new ArrayList<Persona>();
+		var personas = new ArrayList<Persona>();
+			
+		personas.add(Persona.builder()
 				.nombre("Duglas")
 				.primerApellido("Taydron")
 				.segundoApellido("Gonzalez")
 				.fechaNacimiento(LocalDate.of(2001, Month.AUGUST, 15))
 				.genero(Genero.HOMBRE)
-				.build();
+				.build());
 
-		var persona2 = Persona.builder()
+		personas.add(Persona.builder()
 				.nombre("Alex")
 				.primerApellido("Pilicita")
 				.segundoApellido("Changoluisa")
 				.fechaNacimiento(LocalDate.of(1987, Month.AUGUST, 25))
 				.genero(Genero.HOMBRE)
-				.build();
+				.build());
 		
-		var persona3 = Persona.builder()
+		personas.add( Persona.builder()
 				.nombre("Carolina")
 				.primerApellido("Garzon")
 				.segundoApellido("Perez")
 				.fechaNacimiento(LocalDate.of(1993, Month.AUGUST, 5))
 				.genero(Genero.MUJER)
-				.build();
+				.build());
 
-		var persona4 = Persona.builder()
+		personas.add(Persona.builder()
 				.nombre("Jeronimo")
 				.primerApellido("Perez")
 				.segundoApellido("Lopez")
 				.fechaNacimiento(LocalDate.of(1993, Month.MAY, 22))
 				.genero(Genero.HOMBRE)
-				.build();
+				.build());
 		
-		personas.add(persona1);
-		personas.add(persona2);
-		personas.add(persona3);
-		personas.add(persona4);
-	
-		personas = Arrays.asList(persona1,persona2,persona3,persona4);
+		//personas.add(persona1);
+		//personas.add(persona2);
+		//personas.add(persona3);
+		//personas.add(persona4);
+	System.out.println(personas);
+		//personas = Arrays.asList(persona1,persona2,persona3,persona4);
 	}
 	
 }

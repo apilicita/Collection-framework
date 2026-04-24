7eb2aa0c65c4d88360f1f9bc33922e57c6b8a18e
apilicita <lexgemix@gmail.com>
2026-04-24 10:40:27 +0200

    antes de utilizar los constructores de las clases que implementan la interfaz list

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,33 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        
-    }
+
+	public static void main(String[] args) {
+    
+	/*https://docs.oracle.com/javase/tutorial/collections/index.html
+	*/
+    //Coleccion de numeros enteros apartir de un array
+	
+		Integer [] arrayNumerosEnteros= {1,2,3,4,5};
+		List<Integer>  numeroEnteros = Arrays.asList(arrayNumerosEnteros);
+				
+		List<Persona> personas = null;
+		
+		Persona persona1 = Persona.builder()
+				.nombre("Duglas")
+				.primerApellido("Taydron")
+				.segundoApellido("Gonzalez")
+				.fechaNacimiento(LocalDate.of(2001, Month.AUGUST, 15))
+				.genero(Genero.HOMBRE)
+				.build();
+		
+		personas.add(persona1);
+		
+	}
+	
 }
